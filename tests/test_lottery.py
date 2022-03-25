c55c66efbdf6f8190de16e0d5a80c6b485b6182c
oraclown <owen.burton13@gmail.com>
2022-03-25 17:15:58 -0400

    Test contract attributes after deployment

--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -1,11 +1,13 @@
 import pytest
 from brownie import accounts
 from time import time
+from brownie.convert import EthAddress
 
 
 @pytest.fixture(scope="module")
 def lottery(Lottery):
-    return accounts[0].deploy(Lottery, 60*60, 1, 1)
+    # return accounts[0].deploy(Lottery, 60*60, 1, 1)
+    return Lottery.deploy(60*60, 1, 1, {"from": accounts[0]})
 
 
 def test_deploy_lottery(lottery):
@@ -14,11 +16,16 @@ def test_deploy_lottery(lottery):
     assert lottery._name == "Lottery"
     assert lottery.balance() == 0
 
-    assert lottery.ticket_cost() == 1
-    assert lottery.for_the_boyz() == 1
+    assert lottery.ticket_buyers(0) == EthAddress("0x0000000000000000000000000000000000000000")
     assert lottery.num_ticket_buyers() == 0
-
-    # TODO how to test start and end times of the lottery?
+    assert lottery.winner() == EthAddress("0x0000000000000000000000000000000000000000")
+    assert lottery.winner_index() == 0
+    assert lottery.winner_payout() == 0
+    assert lottery.ticket_price() == 1
+    assert abs(lottery.lottery_start()) - int(time()) < 1
+    assert lottery.lottery_end() == lottery.lottery_start() + 60*60
+    assert lottery.for_the_boyz() == 1
+    assert lottery.admin() == accounts[0]
 
 
 def test_buy_ticket(lottery):

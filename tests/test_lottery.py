ebbad3ad969cee56101679068324336171fe1666
oraclown <owen.burton13@gmail.com>
2022-03-25 19:45:00 -0400

    Test buy ticket success & fail

--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -2,12 +2,17 @@ import pytest
 from brownie import accounts
 from time import time
 from brownie.convert import EthAddress
+import brownie
 
 
 @pytest.fixture(scope="module")
 def lottery(Lottery):
     # return accounts[0].deploy(Lottery, 60*60, 1, 1)
     return Lottery.deploy(60*60, 1, 1, {"from": accounts[0]})
+
+    # get block at time of deployment
+    # it will contain the timestamp at the time of deployment
+    # TODO
 
 
 def test_deploy_lottery(lottery):
@@ -16,8 +21,9 @@ def test_deploy_lottery(lottery):
     assert lottery._name == "Lottery"
     assert lottery.balance() == 0
 
+    # TODO: write getter function for entire ticket_buyers array
     assert lottery.ticket_buyers(0) == EthAddress("0x0000000000000000000000000000000000000000")
-    assert lottery.num_ticket_buyers() == 0
+    assert lottery.tickets_bought() == 0
     assert lottery.winner() == EthAddress("0x0000000000000000000000000000000000000000")
     assert lottery.winner_index() == 0
     assert lottery.winner_payout() == 0
@@ -29,11 +35,28 @@ def test_deploy_lottery(lottery):
 
 
 def test_buy_ticket(lottery):
-    pass
+    lottery.buy_ticket({"from": accounts[1], "value": 1})
+
+    assert accounts[1].balance() == 999999999999999999999
+    assert lottery.balance() == 1
+    assert lottery.tickets_bought() == 1
+    assert lottery.ticket_buyers(0) == accounts[1]
 
 
 def test_buy_ticket_fail(lottery):
-    pass
+    with brownie.reverts("purchase underpriced"):
+        lottery.buy_ticket({"from": accounts[1], "value": 0})
+    
+    # Buy all tickets so none left
+    tickets_available = lottery.num_tickets({'from': accounts[1]}).return_value
+    for i in range(tickets_available - lottery.tickets_bought()):
+        lottery.buy_ticket({"from": accounts[1], "value": 1})
+    
+    with brownie.reverts("no more tickets available"):
+        lottery.buy_ticket({"from": accounts[1], "value": 1})
+    
+    # TODO: how to test when number of available tickets == 10**6?
+    # because looping 10**6 times and buying a ticket each time takes forever
 
 
 def test_choose_winner(lottery):

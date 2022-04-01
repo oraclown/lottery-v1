3773a0188d98f6aeb0748a93ac34815e258b0503
oraclown <owen.burton13@gmail.com>
2022-04-01 18:15:05 -0400

    Test choose & pay winner, refactor

--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -3,17 +3,26 @@ from brownie import accounts, chain
 from time import time
 from brownie.convert import EthAddress
 import brownie
+import random
 
 
 LOTTERY_LENGTH = 60*60 # 1 hour
+MAX_TICKETS = 10
+TICKET_PRICE = 1
+ADMIN_FEE = 1
+GROSS_PAYOUT = MAX_TICKETS*TICKET_PRICE
+
+
+def find_account(address):
+    for account in accounts:
+        if account.address == address:
+            return account
+    return None
 
 
 @pytest.fixture(scope="function")
 def lottery(Lottery):
-    return Lottery.deploy(10, 1, LOTTERY_LENGTH, 1, {"from": accounts[0]})
-
-    # TODO: get block at time of deployment
-    # it will contain the timestamp at the time of deployment
+    return Lottery.deploy(MAX_TICKETS, TICKET_PRICE, LOTTERY_LENGTH, ADMIN_FEE, {"from": accounts[0]})
 
 
 def test_deploy_lottery(lottery):
@@ -22,22 +31,22 @@ def test_deploy_lottery(lottery):
     assert lottery._name == "Lottery"
     assert lottery.balance() == 0
 
-    assert lottery.maxTickets() == 10
+    assert lottery.maxTickets() == MAX_TICKETS
     assert lottery.getTicketBuyers() == []
     assert lottery.ticketsBought() == 0
-    assert lottery.ticketPrice() == 1
+    assert lottery.ticketPrice() == TICKET_PRICE
     assert lottery.winner() == EthAddress("0x0000000000000000000000000000000000000000")
     assert abs(lottery.lotteryStart()) - int(time()) < 1
     assert lottery.lotteryEnd() == lottery.lotteryStart() + LOTTERY_LENGTH
-    assert lottery.forTheBoyz() == 1
+    assert lottery.forTheBoyz() == ADMIN_FEE
     assert lottery.admin() == accounts[0]
 
 
 def test_buy_ticket(lottery):
-    lottery.buyTicket({"from": accounts[1], "value": 1})
+    lottery.buyTicket({"from": accounts[1], "value": TICKET_PRICE})
 
     assert accounts[1].balance() == 999999999999999999999
-    assert lottery.balance() == 1
+    assert lottery.balance() == TICKET_PRICE
     assert lottery.ticketsBought() == 1
     assert lottery.ticketBuyers(0) == accounts[1]
 
@@ -50,28 +59,49 @@ def test_buy_ticket_fail(lottery):
     # Try to buy when no tickets left
     tickets_available = lottery.maxTickets() - lottery.ticketsBought()
     for i in range(tickets_available):
-        lottery.buyTicket({"from": accounts[1], "value": 1})
+        lottery.buyTicket({"from": accounts[1], "value": TICKET_PRICE})
     
     with brownie.reverts("No more tickets left"):
-        lottery.buyTicket({"from": accounts[1], "value": 1})
+        lottery.buyTicket({"from": accounts[1], "value": TICKET_PRICE})
     
     # Try to buy when lottery has ended
     chain.sleep(LOTTERY_LENGTH + 1)
     with brownie.reverts("Lottery is over"):
-        lottery.buyTicket({"from": accounts[1], "value": 1})
+        lottery.buyTicket({"from": accounts[1], "value": TICKET_PRICE})
 
 
-def test_choose_winner(lottery):
-    pass
+def test_choose_and_pay_winner(lottery):
+    # Buy all tickets with random accounts
+    for i in range(lottery.maxTickets()):
+        lottery.buyTicket({"from": accounts[random.randint(1, 9)], "value": TICKET_PRICE})
+    min_balance = min([account.balance() for account in accounts])
+
+    assert lottery.balance() == lottery.maxTickets()
+    assert lottery.ticketsBought() == lottery.maxTickets()
+    for buyer in lottery.getTicketBuyers():
+        assert find_account(buyer).balance() < 1000000000000000000000
+        assert buyer != EthAddress("0x0000000000000000000000000000000000000000")
+
+    # Choose winner
+    chain.sleep(LOTTERY_LENGTH + 1)
+    prev_balance = accounts[0].balance()
+    lottery.chooseWinner({"from": accounts[0]})
+
+    assert accounts[0].balance() == prev_balance + TICKET_PRICE
+    assert lottery.winner() != EthAddress("0x0000000000000000000000000000000000000000")
+
+    # Pay winner
+    lottery.payWinner({"from": accounts[0]})
+    winner_balance = find_account(lottery.winner()).balance()
+
+    assert lottery.balance() == 0
+    assert accounts[0].balance() == prev_balance + lottery.ticketPrice()*2 + GROSS_PAYOUT*ADMIN_FEE/100
+    assert winner_balance > min_balance + GROSS_PAYOUT - accounts[0].balance()
 
 
 def test_choose_winner_fail(lottery):
     with brownie.reverts("Lottery not over"):
         lottery.chooseWinner({"from": accounts[0]})
-
-
-def test_pay_winner(lottery):
-    pass
 
 
 def test_pay_winner_fail(lottery):

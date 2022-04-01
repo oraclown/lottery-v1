9f3e193ac357e8f7d9b28e165bfbc10185276c6a
oraclown <owen.burton13@gmail.com>
2022-04-01 08:41:28 -0400

    Test buy after lottery end

--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -1,18 +1,18 @@
 import pytest
-from brownie import accounts
+from brownie import accounts, chain
 from time import time
 from brownie.convert import EthAddress
 import brownie
 
 
+LOTTERY_LENGTH = 60*60 # 1 hour
+
 @pytest.fixture(scope="module")
 def lottery(Lottery):
-    # return accounts[0].deploy(Lottery, 60*60, 1, 1)
-    return Lottery.deploy(10, 1, 60*60, 1, {"from": accounts[0]})
+    return Lottery.deploy(10, 1, LOTTERY_LENGTH, 1, {"from": accounts[0]})
 
-    # get block at time of deployment
+    # TODO: get block at time of deployment
     # it will contain the timestamp at the time of deployment
-    # TODO
 
 
 def test_deploy_lottery(lottery):
@@ -27,7 +27,7 @@ def test_deploy_lottery(lottery):
     assert lottery.ticketPrice() == 1
     assert lottery.winner() == EthAddress("0x0000000000000000000000000000000000000000")
     assert abs(lottery.lotteryStart()) - int(time()) < 1
-    assert lottery.lotteryEnd() == lottery.lotteryStart() + 60*60
+    assert lottery.lotteryEnd() == lottery.lotteryStart() + LOTTERY_LENGTH
     assert lottery.forTheBoyz() == 1
     assert lottery.admin() == accounts[0]
 
@@ -42,11 +42,11 @@ def test_buy_ticket(lottery):
 
 
 def test_buy_ticket_fail(lottery):
-    print('start')
+    # Try to buy a ticket without enough funds
     with brownie.reverts("Ticket purchase underpriced"):
         lottery.buyTicket({"from": accounts[1], "value": 0})
-    print('here')
-    # Buy all tickets so none left
+
+    # Try to buy when no tickets left
     tickets_available = lottery.maxTickets() - lottery.ticketsBought()
     for i in range(tickets_available):
         lottery.buyTicket({"from": accounts[1], "value": 1})
@@ -54,10 +54,10 @@ def test_buy_ticket_fail(lottery):
     with brownie.reverts("No more tickets left"):
         lottery.buyTicket({"from": accounts[1], "value": 1})
     
-    # TODO: how to test when number of available tickets == 10**6?
-    # because looping 10**6 times and buying a ticket each time takes forever
-
-    # TODO: test when lottery is over
+    # Try to buy when lottery has ended
+    chain.sleep(LOTTERY_LENGTH + 1)
+    with brownie.reverts("Lottery is over"):
+        lottery.buyTicket({"from": accounts[1], "value": 1})
 
 
 def test_choose_winner(lottery):
